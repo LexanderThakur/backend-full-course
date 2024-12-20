fee1fd270229d045b055525a19002ee1ff811aa7
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-20 22:47:01 +0530

    crud basics

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -9,19 +9,30 @@ const app = express();
 
 const PORT=8383
 
+let data=["james"];
 //Endpoint- HTTP VERBS(method) && routes()path
 //the method informs the nature of req and 
 // route is further subsirectory,this location are called
 //endpoints
 
+
+
+//Middleware
+app.use(express.json());
+
+
+
+//TYPE 1 Website endpoints (these endpoint are for sending back html)
+//comes when user enters ul in browser
 app.get('/',(req,res)=>{
 
-//this is endpoint number 1- /
-console.log('yay i hit a endpoint',req.method);//like get or post
-res.sendStatus(201);//200-299 ,means succesfull,400 level is error
-//500 means error on server side
 
-
+    res.send(`
+        <body style="background:pink;color:blue">
+        <h1>DATA</h1>
+        <p>${JSON.stringify(data)}</p>
+        <a href="/dashboard">dashboard</a>
+        </body>`);
 
 
 })//this comes before app.listen
@@ -29,8 +40,53 @@ res.sendStatus(201);//200-299 ,means succesfull,400 level is error
 
 
 app.get('/dashboard',(req,res)=>{
-    res.send('<h1>this is a website</h1>');
+    res.send(`
+        <body>
+        <h1>dashboard</h1>
+
+        <a href="/">home</a>
+        </body>
+        
+        
+        `);
 })
+
+//Type2 -API endpoints(non visual) 
+
+
+
+
+//CRUD - create-post,read-get,update-put,delete-delete
+
+
+
+
+
+app.get('/api/data',(req,res)=>{
+    console.log("this is for data");
+    res.status(599).send(data);
+})
+
+app.post('/api/data',(req,res)=>{
+    //someone want to create a user when they click sign up
+    //the user clicks signup after entering cred then the browser sends a network req to hnadle that
+
+    const newEntry=req.body;
+    console.log(newEntry);
+    data.push(newEntry.name);
+    res.sendStatus(201);//201 is for creating
+
+
+})
+
+app.delete('/api/data',(req,res)=>{
+
+    data.pop();
+    console.log("deleted element fom end of arr")
+res.sendStatus(203);
+
+})
+
 
 app.listen(PORT,()=> console.log(`server has started on: ${PORT}`));
 
