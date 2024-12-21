a067e2a0021ffa272fc8c1dfbc06c8f88b6f444b
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-22 01:03:20 +0530

    written code for register and login endpoints

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -5,16 +5,17 @@ import jwt from 'jsonwebtoken'
 import db from '../db.js'
 
 const router=express.Router();
-
+//register a new user end point /auth/register
 router.post('/register',(req,res)=>{
+    const {username,password}=req.body
 
-
-
+console.log(username,password)
+res.sendStatus(201)
 
 })
 
 router.post('/login',(req,res)=>{
-    
+
 })
 
 
