ccb767b1f72b4c2cb4bc99eeee3078add2a5daa3
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-21 23:52:06 +0530

    importing and getting file and directory paths

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,9 +1,28 @@
 import express from 'express'
-
+import path, {dirname} from 'path'
+import{fileURLToPath} from 'url'
 const app=express();
 const PORT= process.env.PORT || 5003
 
-console.log("hello")
+
+
+
+//get file path from url of the current module
+const __filename=fileURLToPath(import.meta.url)
+//get directory name from file path
+const __dirname=dirname(__filename)
+
+
+//serving up the html file from /public dir
+app.get('/',(req,res)=>{
+
+res.sendFile(path.join(__dirname,'public','index.html'));
+
+})
+
+
+
+
 app.listen(PORT,()=>{
 console.log(`Server has started on port ${PORT}`)
 })
