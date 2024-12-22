02fd49f256275e013858d2b064076dff8798b9c6
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-22 22:56:37 +0530

    compeleted login endpoint code

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -35,6 +35,34 @@ router.post('/register',(req,res)=>{
 
 router.post('/login',(req,res)=>{
 
+const {username,password}=req.body
+
+try{
+    const getUser=db.prepare('SELECT * FROM users WHERE username=?')
+    const user=getUser.get(username);
+    //if we cant fine a user in database
+    if(!user){
+        return res.status(404).send({message: "user not found"})
+    }
+    const passwordIsValid=bcrypt.compareSync(password,user.password)//return boolean
+    //check weather password correct
+    if(!passwordIsValid){return res.status(401).send({message: "invalid password"})}
+    //then we have a sucesful auththentication
+
+
+    console.log(user);
+    const token =jwt.sign({id: user.id},process.env.JWT_SECRET,{expiresIn: '24h'})
+    res.json({token});
+
+}catch(err){
+    console.log(err.message);
+    res.sendStatus(503);
+}
+
+
+
+
+
 })
 
 
