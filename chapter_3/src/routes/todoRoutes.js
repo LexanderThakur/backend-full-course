089d342edbc614531e76b1551304c0b0b0a1de8a
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-22 23:14:38 +0530

    written code for authmiddle ware for verifying token for crud changes

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -7,6 +7,10 @@ const router=express.Router();
 //get all to do for logged in user
 router.get('/',(req,res)=>{
 
+    const getTodos=db.prepare('SELECT * FROM todos WHERE user_id=?')
+    const todos=getTodos.all(req.userid)
+    res.json(todos)
+
 })
 
 //create to do
--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -4,6 +4,7 @@ import{fileURLToPath} from 'url'
 
 import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
+import authMiddleware from './middleware/authMiddleware.js'
 
 
 const app=express();
@@ -21,6 +22,7 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname,'../public')));
 
+
 //serving up the html file from /public dir
 app.get('/',(req,res)=>{
 
@@ -29,7 +31,7 @@ res.sendFile(path.join(__dirname,'public','index.html'));
 })
 //routes
 app.use('/auth',authRoutes)
-app.use('/todos',todoRoutes)
+app.use('/todos',authMiddleware,todoRoutes)
 
 
 
