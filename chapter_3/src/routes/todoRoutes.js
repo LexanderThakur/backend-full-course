923f212bc17f6846f107e089269b0290efb6693e
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-22 23:29:20 +0530

    fixed some bug but default todo not loading

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -8,7 +8,7 @@ const router=express.Router();
 router.get('/',(req,res)=>{
 
     const getTodos=db.prepare('SELECT * FROM todos WHERE user_id=?')
-    const todos=getTodos.all(req.userid)
+    const todos=getTodos.all(req.userId)
     res.json(todos)
 
 })
