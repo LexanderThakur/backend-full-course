f26b1724330291d31756c6db3f072de2b17816d6
LexanderThakur <lexanderthakur3@gmail.com>
2024-12-22 22:41:15 +0530

    compelted code for register endpoint , added entry to table using prepare() and used jwt token

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -9,8 +9,27 @@ const router=express.Router();
 router.post('/register',(req,res)=>{
     const {username,password}=req.body
 
-console.log(username,password)
-res.sendStatus(201)
+    //encrypt tthe password
+    const hashedPassword=bcrypt.hashSync(password,8);
+    //save the new user and hasehd pass to db
+    try{
+        const insertUser=db.prepare(`INSERT INTO users (username,password) VALUES(?,?)`)
+        const result=insertUser.run(username,hashedPassword);
+
+        //now we have user i want to add first todo for them
+        const defaultTodo=`Hello :) Add your first todo!`;
+        const insertTodo=db.prepare(`insert into todos (user_id,task) values(?,?)`)
+        insertTodo.run(result.lastInsertRowid,defaultTodo);//gives last inserted primary key
+        //create a token
+
+        const token=jwt.sign({id: result.lastInsertRowid},process.env.JWT_SECRET,{expiresIn: '24h'})//JWT_SECRET defined in .env file
+        res.json({token})//checkout in chatgpt what this line does
+    }catch(err){
+        console.log(err.message);
+        res.sendStatus(503);
+    }
+
+
 
 })
 
